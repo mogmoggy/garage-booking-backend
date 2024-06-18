d5598b7cf4b4fd718cb10761acd23fcb39d2abb0
Emard <e.moghisi@gmail.com>
2024-06-18 17:33:43 +0100

    start server functionality added

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/mogmoggy/garage-booking-backend/api"
+	db "github.com/mogmoggy/garage-booking-backend/db/sqlc"
+	"github.com/mogmoggy/garage-booking-backend/util"
+)
+
 func main() {
+
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatalf("failed to open db connection: %v", err)
+	}
+
+	store := db.NewStore(conn)
+
+	server := api.NewServer(store, *config)
+
+	if err = server.Start(); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
 
 }
